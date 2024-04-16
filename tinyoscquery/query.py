3892b3ae679082e9a14c1aedcc0b72aaeee265a6
catboy <catboy1357a@gmail.com>
2024-04-16 15:54:07 -0400

    docs: Update docstrings and type hints in query.py
    Updated the docstrings and type hints in the query.py file to improve clarity and readability. No functional changes were made, only documentation and type hint enhancements.

--- tinyoscquery/query.py
+++ tinyoscquery/query.py
@@ -1,10 +1,21 @@
 import time
+from typing import Any
 from zeroconf import ServiceBrowser, ServiceInfo, ServiceListener, Zeroconf
 import requests
 
 from .shared.node import OSCQueryNode, OSC_Type_String_to_Python_Type, OSCAccess, OSCHostInfo
 
 class OSCQueryListener(ServiceListener):
+    """
+    Listens for OSCQuery services on the network and maintains a list of discovered services.
+
+    Description
+    -----------
+    This class listens for OSCQuery services on the network using Zeroconf and maintains
+    two dictionaries to track discovered services:
+        - `osc_services` for OSC services 
+        - `oscjson_services` for OSCJSON services.
+    """
 
     def __init__(self) -> None:
         self.osc_services = {}
@@ -13,6 +24,18 @@ class OSCQueryListener(ServiceListener):
         super().__init__()
 
     def remove_service(self, zc: 'Zeroconf', type_: str, name: str) -> None:
+        """
+        Removes a service from the list of discovered services.
+
+        Parameters
+        ----------
+        zc : Zeroconf
+            The Zeroconf instance.
+        type_ : str
+            The type of the service.
+        name : str
+            The name of the service.
+        """
         if name in self.osc_services:
             del self.osc_services[name]
 
@@ -20,12 +43,36 @@ class OSCQueryListener(ServiceListener):
             del self.oscjson_services[name]
 
     def add_service(self, zc: 'Zeroconf', type_: str, name: str) -> None:
+        """
+        Adds a service to the list of discovered services.
+
+        Parameters
+        ----------
+        zc : Zeroconf
+            The Zeroconf instance.
+        type_ : str
+            The type of the service.
+        name : str
+            The name of the service.
+        """
         if type_ == '_osc._udp.local.':
             self.osc_services[name] = zc.get_service_info(type_, name)
         elif type_ == '_oscjson._tcp.local.':
             self.oscjson_services[name] = zc.get_service_info(type_, name)
 
     def update_service(self, zc: 'Zeroconf', type_: str, name: str) -> None:
+        """
+        Updates the information of a discovered service.
+
+        Parameters
+        ----------
+        zc : Zeroconf
+            The Zeroconf instance.
+        type_ : str
+            The type of the service.
+        name : str
+            The name of the service.
+        """
         if type_ == '_osc._udp.local.':
             self.osc_services[name] = zc.get_service_info(type_, name)
         elif type_ == '_oscjson._tcp.local.':
@@ -33,18 +80,55 @@ class OSCQueryListener(ServiceListener):
 
 
 class OSCQueryBrowser(object):
+    """
+    A class for browsing OSCQuery services on the network.
+
+    Description
+    -----------
+    This class allows users to discover OSCQuery services on the network and perform operations
+    such as finding services by name and querying nodes by endpoint address.
+    """
     def __init__(self) -> None:
         self.listener = OSCQueryListener()
         self.zc = Zeroconf()
         self.browser = ServiceBrowser(self.zc, ["_oscjson._tcp.local.", "_osc._udp.local."], self.listener)
 
-    def get_discovered_osc(self):
+    def get_discovered_osc(self) -> list[ServiceInfo]:
+        """
+        Retrieves a list of discovered OSC services.
+
+        Returns
+        -------
+        list[ServiceInfo]
+            A list of discovered OSC services.
+        """
         return [oscsvc[1] for oscsvc in self.listener.osc_services.items()]
 
-    def get_discovered_oscquery(self):
+    def get_discovered_oscquery(self) -> list[ServiceInfo]:
+        """
+        Retrieves a list of discovered OSCQuery services.
+
+        Returns
+        -------
+        list[ServiceInfo]
+            A list of discovered OSCQuery services.
+        """
         return [oscjssvc[1] for oscjssvc in self.listener.oscjson_services.items()]
 
-    def find_service_by_name(self, name):
+    def find_service_by_name(self, name: str) -> ServiceInfo | None:
+        """
+        Finds an OSCQuery service by its name.
+
+        Parameters
+        ----------
+        name : str
+            The name of the service to find.
+
+        Returns
+        -------
+        ServiceInfo or None
+            The discovered service information, if found. Otherwise, returns None.
+        """
         for svc in self.get_discovered_oscquery():
             client = OSCQueryClient(svc)
             if name in client.get_host_info().name:
@@ -52,7 +136,20 @@ class OSCQueryBrowser(object):
 
         return None
 
-    def find_nodes_by_endpoint_address(self, address) -> list[tuple[ServiceInfo, OSCHostInfo, OSCQueryNode]]:
+    def find_nodes_by_endpoint_address(self, address: str) -> list[tuple[ServiceInfo, OSCHostInfo, OSCQueryNode]]:
+        """
+        Finds nodes by their endpoint address.
+
+        Parameters
+        ----------
+        address : str
+            The endpoint address to search for.
+
+        Returns
+        -------
+        list[tuple[ServiceInfo, OSCHostInfo, OSCQueryNode]]
+            A list of tuples containing service information, host information, and queried nodes.
+        """
         svcs = []
         for svc in self.get_discovered_oscquery():
             client = OSCQueryClient(svc)
@@ -67,7 +164,22 @@ class OSCQueryBrowser(object):
 
 
 class OSCQueryClient(object):
-    def __init__(self, service_info) -> None:
+    """
+    Represents a client for interacting with an OSCQuery service.
+    Potential Raises: TypeError, ValueError
+    
+    Description
+    -----------
+    OSCQueryClient provides functionality to communicate with an OSCQuery service. 
+    It allows querying information about specific nodes, retrieving host information, 
+    and constructing OSCQueryNode objects from JSON representations.
+
+    Attributes
+    ----------
+    service_info: ServiceInfo
+        Information about the OSCQuery service.
+    """
+    def __init__(self, service_info: ServiceInfo) -> None:
         if not isinstance(service_info, ServiceInfo):
             raise TypeError("service_info isn't a ServiceInfo class!")
 
@@ -77,14 +189,29 @@ class OSCQueryClient(object):
         self.service_info = service_info
         self.last_json = None
 
-    def _get_query_root(self):
+    def _get_query_root(self) -> str:
+        """Constructs the root URL for querying the OSCQuery service from the ServiceInfo object."""
         return f"http://{self._get_ip_str()}:{self.service_info.port}"
 
-    def _get_ip_str(self):
-        ip_str = '.'.join([str(int(num)) for num in self.service_info.addresses[0]])
-        return ip_str
+    def _get_ip_str(self) -> str:
+        """Converts the ServiceInfo address byte representation of an IP address into a string format."""
+        return '.'.join([str(int(num)) for num in self.service_info.addresses[0]])
 
-    def query_node(self, node="/"):
+    def query_node(self, node: str = "/") -> OSCQueryNode | None:
+        """
+        Retrieves information about a specific node path from the OSCQuery service.
+        Potential Raises: requests.HTTPError
+
+        Parameters
+        ----------
+        node : str, optional
+            The path of the node to query. Defaults to the root node ("/").
+
+        Returns
+        -------
+        OSCQueryNode or None
+            An OSCQueryNode object representing the path queried.
+        """
         url = self._get_query_root() + node
         r = None
         try:
@@ -105,7 +232,17 @@ class OSCQueryClient(object):
         return self._make_node_from_json(self.last_json)
 
 
-    def get_host_info(self):
+    def get_host_info(self) -> OSCHostInfo | None:
+        """
+        Retrieves information about the host from the OSCQuery service.
+        Service can contain details; such as the host name, IP address, port number, and transport protocol.
+        Potential Raises: requests.HTTPError
+
+        Returns
+        -------
+        OSCHostInfo or None
+            An OSCHostInfo object representing the host information if available
+        """
         url = self._get_query_root() + "/HOST_INFO"
         r = None
         try:
@@ -138,7 +275,21 @@ class OSCQueryClient(object):
 
         return hi
 
-    def _make_node_from_json(self, json):
+    def _make_node_from_json(self, json: dict[str, Any]) -> OSCQueryNode:
+        """
+        This private method parses the provided JSON object and creates an OSCQueryNode instance
+        Potential Raises: ValueError
+
+        Parameters
+        ----------
+        json dict[str, Any]
+            The JSON object representing the OSCQueryNode.
+
+        Returns
+        -------
+        OSCQueryNode
+            An OSCQueryNode object constructed from the JSON (dict) representation.
+        """
         newNode = OSCQueryNode()
 
         if "CONTENTS" in json:
