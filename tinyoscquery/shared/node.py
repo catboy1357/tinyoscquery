226095511b18a66290e2fe0a15e1204314b89418
catboy <catboy1357a@gmail.com>
2024-04-16 21:21:22 -0400

    docs: Update docstrings and type hints in node.py
    Updated the clarity of docstrings and type hints in the node.py file. No functional changes were made.

--- tinyoscquery/shared/node.py
+++ tinyoscquery/shared/node.py
@@ -1,9 +1,37 @@
 from enum import IntEnum
 import json
 from json import JSONEncoder
+from typing import Any, List
 
 class OSCNodeEncoder(JSONEncoder):
-    def default(self, o):
+    """
+    Custom JSON encoder for OSCQueryNode and OSCHostInfo objects.
+
+    Description
+    -----------
+    OSCNodeEncoder is a custom JSON encoder designed to serialize 
+    OSCQueryNode and OSCHostInfo objects into JSON format. It overrides the
+    default behavior of JSONEncoder to handle custom serialization of:
+        - OSCQueryNode
+        - OSCHostInfo
+        - Python type objects
+
+    """
+    def default(self, o: Any) -> dict[str, Any] | str:
+        """
+        Overrides the default method of JSONEncoder to customize serialization behavior
+        for specific types.
+
+        Parameters
+        ----------
+        o : OSCQueryNode or OSCHostInfo or type
+            The object to be serialized.
+
+        Returns
+        -------
+        dict or str
+            The serialized representation of the object.
+        """
         if isinstance(o, OSCQueryNode):
             obj_dict = {}
             for k, v in vars(o).items():
@@ -41,13 +69,47 @@ class OSCNodeEncoder(JSONEncoder):
         return json.JSONEncoder.default(self, o)
 
 class OSCAccess(IntEnum):
+    """
+    Enumeration representing access levels for OSC query nodes.
+    """
     NO_VALUE = 0
     READONLY_VALUE = 1
     WRITEONLY_VALUE = 2
     READWRITE_VALUE = 3
 
 class OSCQueryNode():
-    def __init__(self, full_path=None, contents=None, type_=None, access=None, description=None, value=None, host_info=None):
+    """
+    Represents a node in the OSCQuery structure.
+
+    Description
+    -----------
+    OSCQueryNode represents a node in the OSCQuery structure, containing information such as
+    its full path, contents, access type, data type, description, value, and host information.
+    It provides methods for finding subnodes, adding child nodes, converting to JSON format,
+    and iteration over its contents.
+    
+    Attributes
+    ----------
+    full_path : str or None, optional
+        The full path of the node within the OSCQuery structure.
+    contents : list[OSCQueryNode] or None, optional
+        A list of child nodes contained within this node.
+    type_ : list[type] or None, optional
+        The data type(s) associated with the node's value.
+    access : OSCAccess or None, optional
+        The access type of the node, indicating its read and write permissions.
+    description : str or None, optional
+        A description of the node.
+    value : list or None, optional
+        The value(s) associated with the node.
+    host_info : OSCHostInfo or None, optional
+        Information about the host associated with the node.
+    """
+    def __init__(self, full_path: str | None = None,
+                 contents: List['OSCQueryNode'] | None = None,
+                 type_: List[type] | None = None, access: OSCAccess | None = None,
+                 description: str | None = None, value: list | None = None,
+                 host_info: 'OSCHostInfo | None' = None) -> None:
         self.contents = contents
         self.full_path = full_path
         self.access = access
@@ -58,7 +120,20 @@ class OSCQueryNode():
         self.host_info = host_info
 
 
-    def find_subnode(self, full_path):
+    def find_subnode(self, full_path: str) -> 'OSCQueryNode | None':
+        """
+        Finds a subnode within the node's contents based on its full path.
+
+        Parameters
+        ----------
+        full_path : str
+            The full path of the subnode to find.
+
+        Returns
+        -------
+        OSCQueryNode or None
+            The subnode if found, otherwise None.
+        """
         if self.full_path == full_path:
             return self
 
@@ -73,7 +148,16 @@ class OSCQueryNode():
 
         return foundNode
 
-    def add_child_node(self, child):
+    def add_child_node(self, child: 'OSCQueryNode') -> None:
+        """
+        Adds a node as a child node to this node's contents.
+        Potential Raises: NodeError, ValueError
+
+        Parameters
+        ----------
+        child : OSCQueryNode
+            The child node to add.
+        """
         if child == self:
             return
 
@@ -98,32 +182,86 @@ class OSCQueryNode():
         parent.contents.append(child)
 
     
-    def to_json(self):
+    def to_json(self) -> dict[str, Any]:
+        """Converts the node and its contents to a JSON string."""
         return json.dumps(self, cls=OSCNodeEncoder)
 
 
     def __iter__(self):
-            yield self
-            if self.contents is not None:
-                for subNode in self.contents:
-                    yield from subNode
+        """
+        Recursively iterates over the node and its contents
+
+        Yields
+        ------
+        OSCQueryNode
+            The current node being iterated over.
+        """
+        yield self
+        if self.contents is not None:
+            for subNode in self.contents:
+                yield from subNode
 
     def __str__(self) -> str:
+        """
+        Returns a human-readable representation of the node.
+        - @ - Full Path
+        - D - Description
+        - T - Type
+        - V - Value
+        """
         return f'<OSCQueryNode @ {self.full_path} (D: "{self.description}" T:{self.type_} V:{self.value})>'
 
 class NodeError(ValueError):
-    def __init__(self, *args, **kwargs):
+    """
+    Exception raised for errors related to OSCQuery nodes.
+
+    Attributes
+    ----------
+    path : str or None
+        The path of the node where the error occurred, if available.
+    """
+    def __init__(self, *args, **kwargs) -> None:
         self.path = kwargs.pop('path', None)
         super().__init__(*args, **kwargs)
     
-    def __str__(self):
+    def __str__(self) -> str:
         msg = super().__str__()
+        # Add kwargs to the return message
         if self.path is not None:
             msg += f' "{self.path}"'
         return msg
 
 class OSCHostInfo():
-    def __init__(self, name, extensions, osc_ip=None, osc_port=None, osc_transport=None, ws_ip=None, ws_port=None) -> None:
+    """
+    Represents information about an OSCQuery host.
+
+    Description
+    -----------
+    OSCHostInfo provides a convenient way to store and manipulate information
+    about an OSCQuery host. It allows access to various attributes such as the
+    host's name, IP address, port numbers, transport protocols, WebSocket.
+
+    Attributes
+    ----------
+    name : str
+        The name of the host.
+    extensions : list[str]
+        List of extensions supported by the host.
+    osc_ip : str or None, optional
+        The IP address for OSC communication.
+    osc_port : int or None, optional
+        The port number for OSC communication.
+    osc_transport : str or None, optional
+        The transport protocol for OSC communication.
+    ws_ip : str or None, optional
+        The IP address for WebSocket communication.
+    ws_port : int or None, optional
+        The port number for WebSocket communication.
+    """
+    def __init__(self, name: str, extensions: list[str],
+                 osc_ip: str | None = None, osc_port: int | None = None,
+                 osc_transport: str | None = None, ws_ip: str | None = None,
+                 ws_port: int | None = None) -> None:
         self.name = name
         self.osc_ip = osc_ip
         self.osc_port = osc_port
@@ -133,12 +271,28 @@ class OSCHostInfo():
         self.extensions = extensions
 
     def to_json(self) -> str:
+        """Converts the OSCHostInfo object to a JSON string."""
         return json.dumps(self, cls=OSCNodeEncoder)
 
     def __str__(self) -> str:
+        """Converts the OSCHostInfo object to a JSON string."""
         return json.dumps(self, cls=OSCNodeEncoder)
 
-def OSC_Type_String_to_Python_Type(typestr):
+def OSC_Type_String_to_Python_Type(typestr: str) -> List[type]:
+    """
+    Convert an OSC type string to a list of corresponding Python types.
+    Potential Raises: ValueError
+
+    Parameters
+    ----------
+    typestr : str
+        The OSC type string to convert.
+
+    Returns
+    -------
+    list[type]
+        A list of Python types corresponding to the OSC type string.
+    """
     types = []
     for typevalue in typestr:
         if typevalue == '':
@@ -159,7 +313,21 @@ def OSC_Type_String_to_Python_Type(typestr):
     return types
 
 
-def Python_Type_List_to_OSC_Type(types_):
+def Python_Type_List_to_OSC_Type(types_: List[type]) -> str:
+    """
+    Convert a list of Python types to a corresponding OSC type string.
+    Potential Raises: ValueError
+
+    Parameters
+    ----------
+    types_ : list[type]
+        The list of Python types to convert.
+
+    Returns
+    -------
+    str
+        The OSC type string corresponding to the Python types.
+    """
     output = []
     for type_ in types_:
         if type_ == int:
