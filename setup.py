83654909778756ce416fea029509cd19f8324c9b
catboy <catboy1357a@gmail.com>
2024-04-18 03:01:23 -0400

    Version Bump

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="tinyoscquery",
-    version='0.1.2',
+    version='0.1.3',
     description="Quick and dirty python implementation for OSCQuery",
-    author="CyberKitsune",
+    author="CyberKitsune, Catboy",
     packages=['tinyoscquery', 'tinyoscquery.shared'],
     install_requires=['zeroconf', 'requests']
 )
