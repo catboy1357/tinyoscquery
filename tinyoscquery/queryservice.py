catboy1357/tinyoscquery

commit 75f5cb68badfdc4e0e88d2f7a04c7c14fa1a671e

refactor: Updated service registration in OSCQueryService class
Refactored service registration methods in the OSCQueryService class to improve code organization and maintainability. The new _register_service method combines service registration logic into a helper method, simplifying the implementation of service advertisements. Additionally, updated type hints in OSCQueryHTTPHandler and OSCHostInfo.

tinyoscquery/queryservice.py
@@ -1,3 +1,4 @@
+import socket
 import threading
 from typing import Any, Type
 from http.server import SimpleHTTPRequestHandler, HTTPServer
@@ -67,6 +68,7 @@ class OSCQueryService(object):
     def start(self) -> None:
         """
         Start the services required by the class.
+        Potential Raises: ValueError
 
         This method initializes Zeroconf and HTTP services.
         """
@@ -135,28 +137,68 @@ class OSCQueryService(object):
                 new_node.type_ = [type(v) for v in value]
         self.add_node(new_node)
 
+    def _register_service(
+        self, service_name: str, name_suffix: str,
+        port: int, desc: dict[str, int], ip: str = "127.0.0.1"
+    ) -> None:
+        """
+        Registers a service with the provided parameters using Zeroconf.
+        The service will be available for discovery on the network.
+        Potential Raises: ValueError
+
+        Parameters
+        ----------
+        service_name : str
+            The name of the service.
+        name_suffix : str
+            The suffix to be appended to the service name.
+        port : int
+            The port on which the service is running.
+        desc : dict[str, int]
+            A dictionary containing descriptive information about the service.
+        ip : str, optional
+            The IP address on which the service will be available. Default is "127.0.0.1".
+        """
+        if not self._zeroconf:
+            raise ValueError(
+                "Zeroconf was not initialized. Failed to register service."
+            )
+
+        self._zeroconf.register_service(ServiceInfo(
+            service_name,
+            f"{self.serverName}.{service_name}",
+            port, 0, 0, desc,
+            f"{self.serverName}.{name_suffix}",
+            addresses=[socket.inet_pton(socket.AF_INET, ip)]
+        ))
+
     def _startOSCQueryService(self) -> None:
         """Starts the OSCQuery service by registering OSCQuery service information."""
-        oscqsDesc = {'txtvers': 1}
-        oscqsInfo = ServiceInfo(
-            "_oscjson._tcp.local.", "%s._oscjson._tcp.local." % self.serverName, self.httpPort,
-            0, 0, oscqsDesc, "%s.oscjson.local." % self.serverName, addresses=["127.0.0.1"]
+        oscqs_desc = {'txtvers': 1}
+        self._register_service(
+            "_oscjson._tcp.local.",
+            "oscjson.local.",
+            self.httpPort,
+            oscqs_desc
         )
-        self._zeroconf.register_service(oscqsInfo)
 
     def _startHTTPServer(self) -> None:
         """Starts the HTTP server by serving requests forever."""
+        if not self.http_server:
+            raise ValueError(
+                "HTTP Server was not initialized. Failed to serve."
+            )
         self.http_server.serve_forever()
 
     def _advertiseOSCService(self) -> None:
         """Advertises the OSC service by registering OSC service information."""
-        oscDesc = {'txtvers': 1}
-        oscInfo = ServiceInfo(
-            "_osc._udp.local.", "%s._osc._udp.local." % self.serverName, self.oscPort,
-            0, 0, oscDesc, "%s.osc.local." % self.serverName, addresses=["127.0.0.1"]
+        osc_desc = {'txtvers': 1}
+        self._register_service(
+            "_osc._udp.local.",
+            "osc.local.",
+            self.oscPort,
+            osc_desc
         )
-
-        self._zeroconf.register_service(oscInfo)
 
 
 class OSCQueryHTTPServer(HTTPServer):
@@ -193,6 +235,7 @@ class OSCQueryHTTPServer(HTTPServer):
 
 class OSCQueryHTTPHandler(SimpleHTTPRequestHandler):
     """HTTP request handler for OSCQuery service."""
+    query_server: 'OSCQueryService'  # Static type checking
 
     def do_GET(self) -> None:
         """
@@ -206,10 +249,10 @@ class OSCQueryHTTPHandler(SimpleHTTPRequestHandler):
             self.send_header("Content-type", "text/json")
             self.end_headers()
             self.wfile.write(bytes(str(
-                self.server.host_info.to_json()
-                ), 'utf-8'))
+                self.query_server.host_info.to_json()
+            ), 'utf-8'))
             return
-        node = self.server.root_node.find_subnode(self.path)
+        node = self.query_server.root_node.find_subnode(self.path)
         if node is None:
             self.send_response(404)
             self.send_header("Content-type", "text/json")

tinyoscquery/shared/node.py
@@ -299,8 +299,8 @@ class OSCHostInfo():
     ----------
     name : str
         The name of the host.
-    extensions : list[str]
-        List of extensions supported by the host.
+    extensions : dict[str, bool]
+        dictionary of extensions supported by the host.
     osc_ip : str or None, optional
         The IP address for OSC communication.
     osc_port : int or None, optional
@@ -313,7 +313,7 @@ class OSCHostInfo():
         The port number for WebSocket communication.
     """
 
-    def __init__(self, name: str, extensions: list[str],
+    def __init__(self, name: str, extensions: dict[str, bool],
                  osc_ip: str | None = None, osc_port: int | None = None,
                  osc_transport: str | None = None, ws_ip: str | None = None,
                  ws_port: int | None = None) -> None:

commit 240e5f731d1626b1d7427e7ecc9d250c695fcf56

fix: Revert variable name change in OSCQueryHTTPHandler
Reverted the variable name from "query_server" back to "server" to address issues introduced in commit 75f5cb6. The initial change was made due to linter complaints about superclass variable name conflicts. However, this alteration caused unexpected behavior, necessitating a return to the original variable name. This commit ensures compatibility and proper functioning without introducing any breaking changes. (hopefully)

tinyoscquery/queryservice.py
@@ -235,7 +235,9 @@ class OSCQueryHTTPServer(HTTPServer):
 
 class OSCQueryHTTPHandler(SimpleHTTPRequestHandler):
     """HTTP request handler for OSCQuery service."""
-    query_server: 'OSCQueryService'  # Static type checking
+    # Used for static type checking
+    # must be 'server' to use the super class variable
+    server: 'OSCQueryService'  # FIXME # type: ignore
 
     def do_GET(self) -> None:
         """
@@ -249,10 +251,10 @@ class OSCQueryHTTPHandler(SimpleHTTPRequestHandler):
             self.send_header("Content-type", "text/json")
             self.end_headers()
             self.wfile.write(bytes(str(
-                self.query_server.host_info.to_json()
+                self.server.host_info.to_json()
             ), 'utf-8'))
             return
-        node = self.query_server.root_node.find_subnode(self.path)
+        node = self.server.root_node.find_subnode(self.path)
         if node is None:
             self.send_response(404)
             self.send_header("Content-type", "text/json")